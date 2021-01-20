23049c71cdb6e4c82a011df80d41c29873ac8d6a
黄泉路好走 <39544678+xiaoxucong@users.noreply.github.com>
2021-01-20 15:35:33 +0800

    作业题目（周日)第二题

---
+++ Week_02/HttpTest.java
@@ -0,0 +1,51 @@
+package two;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class HttpTest {
+    public static String doPostJson(String url, String json) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            // 创建请求内容
+            StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+            httpPost.setEntity(entity);
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        System.out.println("返回的数据"+resultString);
+        return resultString;
+    }
+
+
+    public static void main(String[] args) {
+        JSONObject json1=new JSONObject();
+        String url="http://localhost:8801";
+        System.out.println(doPostJson(url,json1.toString()));
+    }
+
+}
