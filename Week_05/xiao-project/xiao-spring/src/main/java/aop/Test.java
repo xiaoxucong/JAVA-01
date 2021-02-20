a603d74df6150748a47db0a8ae0e30c46814b9e8
黄泉路好走 <39544678+xiaoxucong@users.noreply.github.com>
2021-02-20 18:08:16 +0800

    第九节 1题 AOP

--- Week_05/xiao-project/xiao-spring/src/main/java/aop/Test.java
+++ Week_05/xiao-project/xiao-spring/src/main/java/aop/Test.java
@@ -1,6 +1,8 @@
 package aop;
 
 public class Test {
+	
+	
     public static void main(String[] args) throws Exception {
         Dog dog = (Dog)new AopProxy().createProxy(new DogA());
         dog.eat();
