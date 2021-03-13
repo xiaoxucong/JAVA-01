f7f4239a1d73aeb482c6788d1dc5842ecaca0a85
黄泉路好走 <39544678+xiaoxucong@users.noreply.github.com>
2021-03-13 10:32:03 +0800

    Add files via upload

--- Week_08/xiao-mysql-split-shardingsphere/db.sql
+++ Week_08/xiao-mysql-split-shardingsphere/db.sql
@@ -19,10 +19,10 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for order_0
 -- ----------------------------
-DROP TABLE IF EXISTS `order_0`;
+DROP TABLE IF EXISTS `t_order_0`;
 CREATE TABLE `t_order_0` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -40,10 +40,10 @@ CREATE TABLE `t_order_0` (
 -- ----------------------------
 -- Table structure for order_1
 -- ----------------------------
-DROP TABLE IF EXISTS `order_1`;
+DROP TABLE IF EXISTS `t_order_1`;
 CREATE TABLE `t_order_1` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -61,10 +61,10 @@ CREATE TABLE `t_order_1` (
 -- ----------------------------
 -- Table structure for order_10
 -- ----------------------------
-DROP TABLE IF EXISTS `order_10`;
+DROP TABLE IF EXISTS `t_order_10`;
 CREATE TABLE `t_order_10` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -82,10 +82,10 @@ CREATE TABLE `t_order_10` (
 -- ----------------------------
 -- Table structure for order_11
 -- ----------------------------
-DROP TABLE IF EXISTS `order_11`;
+DROP TABLE IF EXISTS `t_order_11`;
 CREATE TABLE `t_order_11` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -103,10 +103,10 @@ CREATE TABLE `t_order_11` (
 -- ----------------------------
 -- Table structure for order_12
 -- ----------------------------
-DROP TABLE IF EXISTS `order_12`;
+DROP TABLE IF EXISTS `t_order_12`;
 CREATE TABLE `t_order_12` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -124,10 +124,10 @@ CREATE TABLE `t_order_12` (
 -- ----------------------------
 -- Table structure for order_13
 -- ----------------------------
-DROP TABLE IF EXISTS `order_13`;
+DROP TABLE IF EXISTS `t_order_13`;
 CREATE TABLE `t_order_13` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -145,10 +145,10 @@ CREATE TABLE `t_order_13` (
 -- ----------------------------
 -- Table structure for order_14
 -- ----------------------------
-DROP TABLE IF EXISTS `order_14`;
+DROP TABLE IF EXISTS `t_order_14`;
 CREATE TABLE `t_order_14` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -166,10 +166,10 @@ CREATE TABLE `t_order_14` (
 -- ----------------------------
 -- Table structure for order_15
 -- ----------------------------
-DROP TABLE IF EXISTS `order_15`;
+DROP TABLE IF EXISTS `t_order_15`;
 CREATE TABLE `t_order_15` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -187,10 +187,10 @@ CREATE TABLE `t_order_15` (
 -- ----------------------------
 -- Table structure for order_2
 -- ----------------------------
-DROP TABLE IF EXISTS `order_2`;
+DROP TABLE IF EXISTS `t_order_2`;
 CREATE TABLE `t_order_2` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -208,10 +208,10 @@ CREATE TABLE `t_order_2` (
 -- ----------------------------
 -- Table structure for order_3
 -- ----------------------------
-DROP TABLE IF EXISTS `order_3`;
+DROP TABLE IF EXISTS `t_order_3`;
 CREATE TABLE `t_order_3` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -229,10 +229,10 @@ CREATE TABLE `t_order_3` (
 -- ----------------------------
 -- Table structure for order_4
 -- ----------------------------
-DROP TABLE IF EXISTS `order_4`;
+DROP TABLE IF EXISTS `t_order_4`;
 CREATE TABLE `t_order_4` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -250,10 +250,10 @@ CREATE TABLE `t_order_4` (
 -- ----------------------------
 -- Table structure for order_5
 -- ----------------------------
-DROP TABLE IF EXISTS `order_5`;
+DROP TABLE IF EXISTS `t_order_5`;
 CREATE TABLE `t_order_5` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -271,10 +271,10 @@ CREATE TABLE `t_order_5` (
 -- ----------------------------
 -- Table structure for order_6
 -- ----------------------------
-DROP TABLE IF EXISTS `order_6`;
+DROP TABLE IF EXISTS `t_order_6`;
 CREATE TABLE `t_order_6` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -292,10 +292,10 @@ CREATE TABLE `t_order_6` (
 -- ----------------------------
 -- Table structure for order_7
 -- ----------------------------
-DROP TABLE IF EXISTS `order_7`;
+DROP TABLE IF EXISTS `t_order_7`;
 CREATE TABLE `t_order_7` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -313,10 +313,10 @@ CREATE TABLE `t_order_7` (
 -- ----------------------------
 -- Table structure for order_8
 -- ----------------------------
-DROP TABLE IF EXISTS `order_8`;
+DROP TABLE IF EXISTS `t_order_8`;
 CREATE TABLE `t_order_8` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
@@ -334,10 +334,10 @@ CREATE TABLE `t_order_8` (
 -- ----------------------------
 -- Table structure for order_9
 -- ----------------------------
-DROP TABLE IF EXISTS `order_9`;
+DROP TABLE IF EXISTS `t_order_9`;
 CREATE TABLE `t_order_9` (
   `id` bigint NOT NULL,
-  `userId` bigint DEFAULT NULL COMMENT '用户id',
+  `user_id` bigint DEFAULT NULL COMMENT '用户id',
   `order_no` varchar(50) DEFAULT NULL COMMENT '订单号',
   `sum_price` decimal(10,2) DEFAULT NULL COMMENT '总价格',
   `createtime` datetime DEFAULT NULL COMMENT '创建时间',
